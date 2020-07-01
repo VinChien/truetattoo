b98ad3a371803926b96db0fe4b4d94ce6b1b05d5
jianfan0114@gmail.com <jianfan0114@gmail.com>
2020-07-01 23:20:23 +0800

    進度到process

--- js/all.js
+++ js/all.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-    
+
+    $('.galleryArea').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplayspeed: 1000
+      });
+
 });
