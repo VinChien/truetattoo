8e0b41192af31bf4a6db63fed8fc9337c81af54c
jianfan0114@gmail.com <jianfan0114@gmail.com>
2020-07-02 12:51:21 +0800

    set scrollLeft

--- js/all.js
+++ js/all.js
@@ -1,13 +1,20 @@
 $(document).ready(function () {
 
-    $('.galleryArea').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        autoplay: true,
-        autoplayspeed: 1000
-      });
+  // scrollLeft
+  $('html, body, *').mousewheel(function (e, delta) {
+    this.scrollLeft -= (delta * 150);
+    e.preventDefault();
+  });
+
+  // slick
+  $('.galleryArea').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplayspeed: 1000
+  });
 
 });
