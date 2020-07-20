6d43625bbe8e9278538b1540f2b1bf8ec61ad35f
jianfan0114@gmail.com <jianfan0114@gmail.com>
2020-07-20 22:26:18 +0800

    new intro button

--- js/all.js
+++ js/all.js
@@ -1,9 +1,20 @@
 $(document).ready(() => {
 
   // scrollLeft
-  $('*').mousewheel(function (e, move) {
-    this.scrollLeft -= (move * 800);
-  });
+  // $('*').mousewheel(function (e, move) {
+  //   this.scrollLeft -= (move * 500);
+  // });
+
+  // intro button effect
+  document.querySelector('.button').onmousemove = (e) => {
+
+    const x = e.pageX - e.target.offsetLeft
+    const y = e.pageY - e.target.offsetTop
+
+    e.target.style.setProperty('--x', `${x}px`)
+    e.target.style.setProperty('--y', `${y}px`)
+
+  }
 
   // tooltips
   $('[data-toggle="tooltip"]').tooltip();
