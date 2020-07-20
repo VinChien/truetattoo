5ccd59d1c0552ab183e2adaa882c7c4c53ba3bf1
jianfan0114@gmail.com <jianfan0114@gmail.com>
2020-07-20 22:53:03 +0800

    Add smooth scrolling to all links

--- js/all.js
+++ js/all.js
@@ -1,5 +1,28 @@
 $(document).ready(() => {
 
+  // Add smooth scrolling to all links
+  $("a").on('click', function (e) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      e.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
   // scrollLeft
   $('*').mousewheel(function (e, move) {
     this.scrollLeft -= (move * 500);
